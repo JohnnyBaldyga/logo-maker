4666f65d44ca3f507f3836993e990217f9078006
JohnnyBaldyga <jonathanbaldyga.com>
2024-06-30 15:47:16 -0400

    add questions

--- Index.js
+++ Index.js
@@ -1,2 +1,28 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+const questions = [
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters:',
+      validate: input => input.length <= 3 || 'Text must be 3 characters or less',
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter text color:',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter shape color:',
+    },
+  ];
+  
